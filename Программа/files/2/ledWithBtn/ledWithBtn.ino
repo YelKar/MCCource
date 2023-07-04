ec0d6d59e7b188cbae2a89bc87cb49bba1092e9c
YelKar <karamyshev_e@1060.ru>
2023-07-04 18:39:59 +0300

    Добавил примеры схем

--- Программа/files/2/ledWithBtn/ledWithBtn.ino
+++ Программа/files/2/ledWithBtn/ledWithBtn.ino
@@ -1,6 +1,6 @@
 #define btn 2
 
-int last = -1;
+int val = 0;
 
 void setup() {
     Serial.begin(9600);
@@ -8,11 +8,14 @@ void setup() {
     pinMode(btn, INPUT_PULLUP);
 }
 void loop() {
-    int val = !digitalRead(btn);  // Если btn возвращает 1, на светодиод выводим 0
-    digitalWrite(LED_BUILTIN, val);
-    if (val != last) {
-        Serial.print("Светодиод ");
-        Serial.println(val ? "Включен" : "Выключен");
-        last = val;
-    }
+    while (digitalRead(btn));
+    delay(50);
+    while (!digitalRead(btn));
+
+    digitalWrite(LED_BUILTIN, val = !val);
+
+    Serial.print("Светодиод ");
+    Serial.println(val ? "Включен" : "Выключен");
+
+    delay(50);
 }
--- Программа/files/2/leds/leds.ino
+++ Программа/files/2/leds/leds.ino
@@ -3,6 +3,7 @@ int leds[] = {10, 11, 12};  // Указываем выводы для свето
 int val = 0;
 
 void setup() {
+  Serial.begin(9600);
     for (int led : leds) {
         pinMode(led, OUTPUT);  // Устанавливаем пины на выход
     }
@@ -10,7 +11,13 @@ void setup() {
 void loop() {
     for (int i = 0; i < 3; i++) {
         digitalWrite(leds[i], val == i);
+        Serial.print("LED-");
+        Serial.print(leds[i]);
+        Serial.print(":\t");
+        Serial.print(val == i ? "on" : "off");
+        Serial.print("\t");
     }
+    Serial.println();
     val = ++val % 3;
     delay(500);
 }
